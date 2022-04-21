79e3c10f43af2999b13db14b7426132f3992d500
Yevhen Ladiukov <sailormeds@gmail.com>
2022-04-21 09:54:35 +0300

    First

---
+++ app/src/db.py
@@ -0,0 +1,39 @@
+import os
+
+import boto3
+from dotenv import load_dotenv
+from sqlmodel import SQLModel, Session, create_engine
+
+connect_args = {"check_same_thread": False}
+
+load_dotenv()
+# rds_host = os.environ.get("AWS_RDS_HOST")
+# rds_port = int(os.environ.get("AWS_RDS_PORT"))
+# rds_username = os.environ.get("AWS_RDS_USER")
+# rds_db = os.environ.get("AWS_RDS_DB")
+#
+# temp_passwd = boto3.client('rds').generate_db_auth_token(
+#     DBHostname=rds_host,
+#     Port=rds_port,
+#     DBUsername=rds_username
+# )
+#
+# rds_credentials = {'user': rds_username, 'passwd': temp_passwd}
+# connect_args.update(rds_credentials)
+# db_conn_string = f"postgresql://{rds_host}:{rds_port}/{rds_db}"
+
+#SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@db:5432/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:kombucha@db:5432/dev_db"
+#SQLALCHEMY_DATABASE_URL = "postgresql://username:password@db:5432/nudges"
+
+#engine = create_engine(db_conn_string, connect_args=connect_args)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
